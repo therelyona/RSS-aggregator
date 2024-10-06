d4f6551d12523d9da3fa4f8057bf74fbe945207b
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-06 20:50:14 +0400

    modified package-lock.json, package.json, webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ export default {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
     clean: true,
+    path: path.resolve(process.cwd(), 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
@@ -22,26 +22,18 @@ export default {
         },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
       },
       {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        test: /\.(woff2?|ttf|eot|svg)(\?[\s\S]+)?$/,
         use: 'file-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: 'index.html',
     }),
   ],
 };
