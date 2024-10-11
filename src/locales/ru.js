fe9b124e81f9c11d860ce18bdc0c76fdae29db79
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-11 20:14:17 +0400

    added src/locales/

---
+++ src/locales/ru.js
@@ -0,0 +1,9 @@
+export default {
+  translation: {
+    notURL: 'Ссылка должна быть валидным URL',
+    duplicateUrl: 'RSS уже существует',
+    networkError: 'Ошибка сети',
+    parsingError: 'Ресурс не содержит валидный RSS',
+    success: 'RSS успешно загружен',
+  },
+};
