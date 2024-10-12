aea1378eaeee03fa8e825f3fb772a8002c406295
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-12 22:04:41 +0400

    modified src/view.js

--- src/view.js
+++ src/view.js
@@ -1,53 +1,53 @@
-const renderErrors = (elements, errors, i18nInstance) => {
-  const { feedback, input, form } = elements;
+const renderErrors = (elements, errors) => {
+  const {
+    feedback,
+    input,
+    form,
+    submit,
+  } = elements;
 
-  if (errors.url) {
-    feedback.textContent = i18nInstance.t(errors.url);
-    input.classList.add('is-invalid');
-    feedback.classList.add('text-danger');
-    form.reset();
-    input.focus();
-  } else {
-    input.classList.remove('is-invalid');
-    feedback.classList.remove('text-danger');
-    input.classList.add('is-valid');
-    feedback.classList.add('text-success');
-    feedback.textContent = i18nInstance.t('success');
-    form.reset();
-    input.focus();
-  }
+  feedback.textContent = errors.url;
+  input.classList.add('is-invalid');
+  feedback.classList.add('text-danger');
+  submit.disabled = false;
+  form.reset();
+  input.focus();
 };
 
-const renderProcessState = (elements, process) => {
-  const { submit, input } = elements;
-  switch (process) {
-    case 'filling':
-      submit.disabled = false;
-      input.disabled = false;
-      break;
-    case 'sending':
-      submit.disabled = true;
-      input.disabled = true;
-      break;
-    case 'finished':
-      submit.disabled = false;
-      input.disabled = true;
-      break;
-    case 'failed':
-      submit.disabled = false;
-      break;
-    default:
-      throw new Error(`Неизвестный процесс ${process}`);
-  }
+const renderSuccess = (elements, i18n) => {
+  const {
+    feedback,
+    input,
+    form,
+    submit,
+  } = elements;
+
+  input.classList.remove('is-invalid');
+  feedback.classList.remove('text-danger');
+  input.classList.add('is-valid');
+  feedback.classList.add('text-success');
+  feedback.textContent = i18n.t('success');
+  submit.disabled = false;
+  form.reset();
+  input.focus();
 };
 
-const view = (state, elements, i18nInstance) => (path, value) => {
+const view = (state, elements, i18n) => (path, value) => {
+  const { submit } = elements;
   switch (path) {
-    case 'form.errors':
-      renderErrors(elements, value, i18nInstance);
-      break;
-    case 'process.processState':
-      renderProcessState(elements, value);
+    case 'form.processState':
+      if (value === 'filling') {
+        submit.disabled = false;
+      }
+      if (value === 'sending') {
+        submit.disabled = true;
+      }
+      if (value === 'finished') {
+        renderSuccess(elements, i18n);
+      }
+      if (value === 'failed') {
+        renderErrors(elements, state.errors);
+      }
       break;
     default:
       break;
