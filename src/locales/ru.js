d0c9a853731385e60637c3a627ae09fb528d129b
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-12 22:04:06 +0400

    add new translation

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,9 +1,14 @@
 export default {
   translation: {
-    notURL: 'Ссылка должна быть валидным URL',
-    duplicateUrl: 'RSS уже существует',
-    networkError: 'Ошибка сети',
-    parsingError: 'Ресурс не содержит валидный RSS',
+    errors: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      duplicateUrl: 'RSS уже существует',
+      networkError: 'Ошибка сети',
+      parsingError: 'Ресурс не содержит валидный RSS',
+      required: 'Не должно быть пустым',
+    },
     success: 'RSS успешно загружен',
+    feeds: 'Фиды',
+    posts: 'Посты',
   },
 };
