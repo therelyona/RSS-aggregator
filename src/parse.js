4e00e968df36e30ac1d09d54240ec3876517c86d
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-10-18 14:20:55 +0400

    function refactoring for universality

--- src/parse.js
+++ src/parse.js
@@ -5,18 +5,14 @@ export default (response) => {
   const parserError = doc.querySelector('parsererror');
   if (parserError) throw new Error('parsingError');
 
-  const feed = {
-    title: doc.querySelector('title').textContent,
-    description: doc.querySelector('description').textContent,
-    link: doc.querySelector('link').textContent,
-  };
+  const title = doc.querySelector('title').textContent;
+  const description = doc.querySelector('description').textContent;
 
-  const feedItems = doc.querySelectorAll('item');
-  const posts = [...feedItems].map((item) => ({
+  const items = [...doc.querySelectorAll('item')].map((item) => ({
     title: item.querySelector('title').textContent,
     description: item.querySelector('description').textContent,
     link: item.querySelector('link').textContent,
   }));
 
-  return { feed, posts };
+  return { title, description, items };
 };
